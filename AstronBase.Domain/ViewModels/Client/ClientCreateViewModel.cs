ce08b2004d7d21f60f79e56b5f812d8943c7d755
s.bury <s.bury@LOOK>
2023-08-03 23:34:24 +0300

    refactor

---
+++ AstronBase.Domain/ViewModels/Client/ClientCreateViewModel.cs
@@ -0,0 +1,23 @@
+﻿using AstronBase.Domain.ViewModels.Pagination;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstronBase.Domain.ViewModels.Client
+{
+    public class ClientCreateViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int CompanyId { get; set; }
+        public string PhoneNumber { get; set; }
+
+        //public IEnumerable Clients { get; }
+        //public PageViewModel PageViewModel { get; }
+    }
+}
---
+++ AstronBase/Controllers/RoleController.cs
@@ -0,0 +1,140 @@
+﻿using AstronBase.Domain.ViewModels.Roles;
+using AstronBase.Service.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+
+namespace AstronBase.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly IRoleService _roleService;
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public RoleController(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        /// <summary>
+        /// [Get] Метод, создания тега
+        /// </summary>
+        [Route("Role/Create")]
+        [Authorize(Roles = "Администратор, Модератор")]
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// [Post] Метод, создания тега
+        /// </summary>
+        [Route("Role/Create")]
+        [Authorize(Roles = "Администратор, Модератор")]
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(RoleCreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var roleId = await _roleService.CreateRole(model);
+
+                _logger.Info($"Созданна роль - {model.Name}");
+
+                return RedirectToAction("GetRoles", "Role");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Некорректные данные");
+
+                _logger.Error($"Попытка создания роли {model.Name} неуспешна");
+
+                return View(model);
+            }
+        }
+
+        /// <summary>
+        /// [Get] Метод, редактирования тега
+        /// </summary>
+        [Route("Role/Edit")]
+        [Authorize(Roles = "Администратор, Модератор")]
+        [HttpGet]
+        public async Task<IActionResult> EditRole(Guid id)
+        {
+            var role = _roleService.GetRole(id);
+
+            var view = new RoleEditViewModel { Id = id, Description = role.Result?.Description, Name = role.Result?.Name };
+
+            return View(view);
+        }
+
+        /// <summary>
+        /// [Post] Метод, редактирования тега
+        /// </summary>
+        [Route("Role/Edit")]
+        [Authorize(Roles = "Администратор, Модератор")]
+        [HttpPost]
+        public async Task<IActionResult> EditRole(RoleEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _roleService.EditRole(model);
+
+                _logger.Info($"Измененна роль - {model.Name}");
+
+                return RedirectToAction("GetRoles", "Role");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Некорректные данные");
+
+                _logger.Error($"попытка изменения роли {model.Name} неуспешна");
+
+                return View(model);
+            }
+        }
+
+        /// <summary>
+        /// [Get] Метод, удаления тега
+        /// </summary>
+        [Route("Role/Remove")]
+        [Authorize(Roles = "Администратор, Модератор")]
+        [HttpGet]
+        public async Task<IActionResult> RemoveRole(Guid id, bool isConfirm = true)
+        {
+            if (isConfirm)
+
+                await RemoveRole(id);
+
+            return RedirectToAction("GetRoles", "Role");
+        }
+
+        /// <summary>
+        /// [Post] Метод, удаления тега
+        /// </summary>
+        [Route("Role/Remove")]
+        [Authorize(Roles = "Администратор, Модератор")]
+        [HttpPost]
+        public async Task<IActionResult> RemoveRole(Guid id)
+        {
+            await _roleService.RemoveRole(id);
+
+            _logger.Info($"Удаленна роль - {id}");
+
+            return RedirectToAction("GetRoles", "Role");
+        }
+
+        /// <summary>
+        /// [Get] Метод, получения всех тегов
+        /// </summary>
+        [Route("Role/GetRoles")]
+        [HttpGet]
+        [Authorize(Roles = "Администратор, Модератор")]
+        public async Task<IActionResult> GetRoles()
+        {
+            var roles = await _roleService.GetRoles();
+
+            return View(roles);
+        }
+    }
+}
